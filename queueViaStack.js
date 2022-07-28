2f969b40343e69b4f5e573c7c71984f38ef70a36
Avi <aviarchi1994@gmail.com>
2022-07-28 18:24:30 +0300

    queue via two stack

--- queueViaStack.js
+++ queueViaStack.js
@@ -1,28 +1,47 @@
 // implement queue by using two stack
 const Stack = require("./Stack");
 
-export class SpecialQueue {
-  stack1 = new Stack();
-  stack2 = new Stack();
+class SpecialQueue {
+  stack1;
+  stack2;
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
   add(i) {
     let j;
-    while (!this.stack1.isEmpty) {
+    while (!this.stack1.isEmpty()) {
       j = this.stack1.pop();
       this.stack2.add(j);
     }
-    this.stack2.add(i);
-
-    this.stack2.add(i);
-    while (!this.stack2.isEmpty) {
+    this.stack1.add(i);
+    while (!this.stack2.isEmpty()) {
       j = this.stack2.pop();
       this.stack1.add(j);
     }
   }
 
   pick() {
-    return this.stack1.pick;
+    return this.stack1.pick();
   }
   pop() {
-    return this.stack1.pop;
+    return this.stack1.pop();
   }
 }
+
+const main = () => {
+  const myQueue = new SpecialQueue();
+  myQueue.add(1);
+  myQueue.add(2);
+  myQueue.add(14);
+  myQueue.add(16);
+  myQueue.add(11);
+  myQueue.add(41);
+  myQueue.add(7);
+  console.log(myQueue.pop()); //1
+  console.log(myQueue.pop()); //2
+  console.log(myQueue.pick()); //14
+  console.log(myQueue.pop()); //14
+  console.log(myQueue.pick()); //16
+};
+module.exports = main;
